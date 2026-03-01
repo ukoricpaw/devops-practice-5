baa387a770cd8863a14d93400287b0b80b11353b
ukoricpaw <phero8121324@gmail.com>
2026-03-01 20:15:25 +0500

    Added postgres support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT || 5432),
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      entities: [],
+      database: 'pgWithNest',
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
